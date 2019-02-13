7025e18dd0bb1bff3cff76c7f1c57a7851277d23
AddisonJi <45214413+AddisonJi@users.noreply.github.com>
2019-02-12 18:37:13 -0800

    Update main.py
    add comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,7 +13,8 @@ with open(pypoll_csv, newline="") as csvfile:
 
     # Read the header row first (skip this part if there is no header)
     csv_header = next(csvfile)
-
+    
+# create a dictionary, if the candidate exist in the key then, the value increase 1, if not then start the value as 1
     for row in csvreader:
         totalvotes+=1
         if row[2] in summary.keys():
@@ -25,20 +26,23 @@ with open(pypoll_csv, newline="") as csvfile:
 
     print("---------------------------------------------")
     output.writelines("---------------------------------------------\n")
-
+#total votes is the number of the line
     print(f"Total Votes: {totalvotes}")
     output.writelines(f"Total Votes: {totalvotes}\n")
 
     print("---------------------------------------------")
     output.writelines("---------------------------------------------\n")
 
+#loop thru four keys in the dictionary, print out the format in the homework instruction
     for key in summary:
         print (key, ":",round(((summary[key]/totalvotes)*100),3) ,"%", "(",summary[key],")")
 
         a=key, ": ",str(round(((summary[key]/totalvotes)*100),3)) ,"% ", "(",str(summary[key]),")\n"
         output.writelines(a)
-
+# create another list to store the votes, because I want to use the max function on the list later
         votes.append(summary[key])
+    
+#print out the homework instruction layout      
     print("---------------------------------------------")
     output.writelines("---------------------------------------------\n")
     for key in summary:
